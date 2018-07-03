282a29502f15f869152124eb86759ce863e6532a
Aleksei <fomin.a@liceum1535.ru>
2018-07-03 16:30:41 +0300

    Те файлы которые нужно сохранять(ВРОДЕ)

---
+++ Desktop/Bot Application1/Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+        
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+        
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+            await context.PostAsync("Приветик, давай сыграем с тобой в крестики нолики?");
+            // return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            if (activity.Text == "Давай")
+            {
+                await context.PostAsync("0|_A_|_B_|_C_|\n1|___|___|___|\n2|___|___|___|\n3|___|___|___|\nВведи координату");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
